ad8f78bf8ae50833d4384e6f3eeaaf626c8f8884
apo-eco-rails-dev <apo.eco_rails_dev@yahoo.com>
2013-07-19 00:11:06 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexBook::Application.config.secret_token = 'c66b50ce6a3854178f895231fcc024d954b1d8ac2e2e934dc78a079fe75e88f2d8effe9fd745023de720cfe870cc53ba329751db05ecf12d727768d5ed70ed50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AlexBook::Application.config.session_store :cookie_store, key: '_AlexBook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AlexBook::Application.config.session_store :active_record_store
